9994be01e93a98f479708acc8cc67740e091555e
alexspirou <68375963+alexspirou@users.noreply.github.com>
2022-06-09 23:27:45 +0300

    minor changes

--- Monster_Truck_UI/src/Manual_Window/manual_window.cpp
+++ Monster_Truck_UI/src/Manual_Window/manual_window.cpp
@@ -64,33 +64,25 @@ void Manual_Window::keyPressEvent(QKeyEvent *event){
 void Manual_Window::keyReleaseEvent(QKeyEvent *event){
     switch(event->key()) {
         case Qt::Key_8:
-            //forward
             ui->forward_label->setStyleSheet("");
-            ros_f->set_cmd_vel_msg(0);
+            ros_f->set_cmd_vel_msg(STOP);
             ros_f->cmd_vel_publisher();
-            qDebug() << "released" ;
-
             break;
         case Qt::Key_2:
-            //backward
             ui->back_label->setStyleSheet("");
-            ros_f->set_cmd_vel_msg(0);
+            ros_f->set_cmd_vel_msg(STOP);
             ros_f->cmd_vel_publisher();
             break;
         case Qt::Key_6:
-            //right
-            ros_f->servo_command_publisher(0);
-            //rviz_obj->turn_right_released();
+            ros_f->servo_command_publisher(STOP);
             ui->right_label->setStyleSheet("");
             break;
         case Qt::Key_4:
             //left
-            ros_f->servo_command_publisher(0);
-            //rviz_obj->turn_left_released();
+            ros_f->servo_command_publisher(STOP);
             ui->left_label->setStyleSheet("");
             break;
         case Qt::Key_Z:
-            qDebug() << "stop";
             break;
         }
 
